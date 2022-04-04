41e9c25ccaec0d727b0000df263123b9f0b9e814
Shama <assiddik001@gmail.com>
2022-04-04 16:36:18 +0600

    Router setup

---
+++ src/components/Dashborad/Dashborad.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Dashborad.css'
+
+const Dashborad = () => {
+    return (
+        <div>
+            <h1>Dashboard</h1>
+        </div>
+    );
+};
+
+export default Dashborad;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Header.css";
+import { Navbar } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
+import { Nav } from 'react-bootstrap';
+import CustomLink from '../CustomLink/CustomLink';
+
+
+const Header = () => {
+  return (
+    <nav>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mx-auto my-4">
+              <CustomLink to="/">Home</CustomLink>
+              <CustomLink to="/reviews">Reviews</CustomLink>
+              <CustomLink to="/dashboard">Dashboard</CustomLink>
+              <CustomLink to="/blogs">Blogs</CustomLink>
+              <CustomLink to="/about">About</CustomLink>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </nav>
+  );
+};
+
+export default Header;
