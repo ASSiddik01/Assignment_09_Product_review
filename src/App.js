98e9b2ad1dfb3f35d072a48a151ed554adc0bde5
Shama <assiddik001@gmail.com>
2022-04-04 17:55:42 +0600

    Banner done

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Reviews from './components/Reviews/Reviews';
 
 function App() {
   return (
-    <div className="App">
+    <div className="">
       <Header></Header>
       <Routes>
         <Route path='/' element={<Home></Home> } ></Route>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,37 @@
-import React from 'react';
-import './Home.css';
+import React from "react";
+import { Button, Container, Image, Row } from "react-bootstrap";
+import "./Home.css";
+import banner from "../../image/banner.png";
+import logo from "../../image/logo.png";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+  import { faArrowRightLong } from '@fortawesome/free-solid-svg-icons'
 
 const Home = () => {
-    return (
-        <div>
-            <h1>HOME</h1>
-        </div>
-    );
+  return (
+    <div>
+      <Container>
+        <Row className="my-3  align-items-center">
+          <div className="col-lg-6 col-md-6">
+            <div className="banner_info d-flex align-items-center">
+              <img className="" width={50} src={logo} alt="" />
+              <h1 className="text-uppercase fw-bold text-info mb-0 mt-3 ms-3">Hackintosh</h1>
+            </div>
+            <h1 className=" fw-bold ">MacBook Killer !</h1>
+                      <h1 className=" fw-bold text-warning">Lenovo ThinkPad X240</h1>
+                      <p>I think <span>Lenovo ThinkPad X240</span> MacBook killer Laptop. Laptop Configuration is; Processor - i5 4300U, Ram - 8GB DDR3L, SSD - 120GB. <b>But funny thing is the laptop is mine and I install hackintosh in this laptop and also put the Apple logo from my old Macbook.</b> </p>
+                      <Button variant="info">Read More <FontAwesomeIcon icon={faArrowRightLong} /> </Button>
+          </div>
+          <div className="col-lg-6 col-md-6">
+            <Image
+              className="img-fluid border rounded-circle pt-5"
+              src={banner}
+              alt=""
+            />
+          </div>
+        </Row>
+      </Container>
+    </div>
+  );
 };
 
 export default Home;
